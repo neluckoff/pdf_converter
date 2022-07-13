f06995b7a6e3497081e7210818efaa659aa7383e
luckoff <neluckoff@gmail.com>
2022-07-13 22:36:00 +0300

    Added new file save

--- misc/pdf_work.py
+++ misc/pdf_work.py
@@ -9,67 +9,74 @@ from tkinter import messagebox as mb
 from misc.text_wrapper import TextWrapper
 
 
-def choose_funct(choosing, path, text):
+def choose_funct(choosing, path, text, save_path):
     """Choosing menu"""
     if choosing == "Get information about a PDF file":
         pdf_info(path, text)
-    elif choosing == "Convert PDF to DOCX":
-        pdf_to_docx(path, text)
-    elif choosing == "Delete page from PDF":
-        new_app = Tk()
-        new_app.title("Delete page from PDF")
-        new_app.geometry('220x100')
-        x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
-        y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
-        new_app.wm_geometry("+%d+%d" % (x, y))
-        var1 = StringVar(new_app)
-        entry = Entry(new_app, textvariable=var1)
-        entry.grid(row=0, column=1)
-        Button(new_app, text='Delete', command=lambda: pdf_delete_page(path, int(entry.get()), new_app, text))\
-            .grid(row=0, column=2)
-        new_app.mainloop()
-    elif choosing == "Select one page from PDF":
-        new_app = Tk()
-        new_app.title("Select one page from PDF")
-        new_app.geometry('220x100')
-        x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
-        y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
-        new_app.wm_geometry("+%d+%d" % (x, y))
-        var1 = StringVar(new_app)
-        entry = Entry(new_app, textvariable=var1)
-        entry.grid(row=0, column=1)
-        Button(new_app, text='Select', command=lambda: pdf_select_page(path, int(entry.get()), new_app, text))\
-            .grid(row=0, column=2)
-        new_app.mainloop()
-    elif choosing == "Merge two PDF files":
-        new_file = fd.askopenfilename()
-        merge_pdf(path, new_file, text)
-    elif choosing == "Encrypt PDF file":
-        new_app = Tk()
-        new_app.title("Encrypt PDF file")
-        new_app.geometry('220x100')
-        x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
-        y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
-        new_app.wm_geometry("+%d+%d" % (x, y))
-        var1 = StringVar(new_app)
-        entry = Entry(new_app, textvariable=var1)
-        entry.grid(row=0, column=1)
-        Button(new_app, text='Encrypt', command=lambda: encrypt_pdf(path, entry.get(), new_app, text)) \
-            .grid(row=0, column=2)
-        new_app.mainloop()
-    elif choosing == "Decrypt PDF file":
-        new_app = Tk()
-        new_app.title("Decrypt PDF file")
-        new_app.geometry('220x100')
-        x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
-        y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
-        new_app.wm_geometry("+%d+%d" % (x, y))
-        var1 = StringVar(new_app)
-        entry = Entry(new_app, textvariable=var1)
-        entry.grid(row=0, column=1)
-        Button(new_app, text='Decrypt', command=lambda: decrypt_pdf(path, entry.get(), new_app, text)) \
-            .grid(row=0, column=2)
-        new_app.mainloop()
+    elif save_path is not None:
+        if choosing == "Convert PDF to DOCX":
+            pdf_to_docx(path, text, save_path)
+        elif choosing == "Delete page from PDF":
+            new_app = Tk()
+            new_app.title("Delete page from PDF")
+            new_app.geometry('220x100')
+            x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
+            y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
+            new_app.wm_geometry("+%d+%d" % (x, y))
+            var1 = StringVar(new_app)
+            entry = Entry(new_app, textvariable=var1)
+            entry.grid(row=0, column=1)
+            Button(new_app, text='Delete', command=lambda: pdf_delete_page(path, int(entry.get()),
+                                                                           new_app, text, save_path))\
+                .grid(row=0, column=2)
+            new_app.mainloop()
+        elif choosing == "Select one page from PDF":
+            new_app = Tk()
+            new_app.title("Select one page from PDF")
+            new_app.geometry('220x100')
+            x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
+            y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
+            new_app.wm_geometry("+%d+%d" % (x, y))
+            var1 = StringVar(new_app)
+            entry = Entry(new_app, textvariable=var1)
+            entry.grid(row=0, column=1)
+            Button(new_app, text='Select', command=lambda: pdf_select_page(path, int(entry.get()), new_app,
+                                                                           text, save_path))\
+                .grid(row=0, column=2)
+            new_app.mainloop()
+        elif choosing == "Merge two PDF files":
+            new_file = fd.askopenfilename()
+            merge_pdf(path, new_file, text, save_path)
+        elif choosing == "Encrypt PDF file":
+            new_app = Tk()
+            new_app.title("Encrypt PDF file")
+            new_app.geometry('220x100')
+            x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
+            y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
+            new_app.wm_geometry("+%d+%d" % (x, y))
+            var1 = StringVar(new_app)
+            entry = Entry(new_app, textvariable=var1)
+            entry.grid(row=0, column=1)
+            Button(new_app, text='Encrypt', command=lambda: encrypt_pdf(path, entry.get(),
+                                                                        new_app, text, save_path)) \
+                .grid(row=0, column=2)
+            new_app.mainloop()
+        elif choosing == "Decrypt PDF file":
+            new_app = Tk()
+            new_app.title("Decrypt PDF file")
+            new_app.geometry('220x100')
+            x = (new_app.winfo_screenwidth() - new_app.winfo_reqwidth()) / 2
+            y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
+            new_app.wm_geometry("+%d+%d" % (x, y))
+            var1 = StringVar(new_app)
+            entry = Entry(new_app, textvariable=var1)
+            entry.grid(row=0, column=1)
+            Button(new_app, text='Decrypt', command=lambda: decrypt_pdf(path, entry.get(),
+                                                                        new_app, text, save_path)) \
+                .grid(row=0, column=2)
+            new_app.mainloop()
+    else:
+        mb.showerror("ERROR", "Specify the path to save the file!")
 
 
 def pdf_info(path, text):
@@ -90,7 +97,7 @@ def pdf_info(path, text):
         mb.showerror("INFO", "File not found!")
 
 
-def pdf_select_page(path, page, app, text):
+def pdf_select_page(path, page, app, text, save_path):
     """Extract a page from a PDF file into a separate file"""
     app.destroy()
     file_name = os.path.splitext(os.path.basename(path))[0]
@@ -103,7 +110,7 @@ def pdf_select_page(path, page, app, text):
             pdf_writer.addPage(pdf.getPage(page-1))
 
             output_filename = f'{file_name}_page_{page}.pdf'
-            with open(output_filename, 'wb') as out:
+            with open(f'{save_path}/{output_filename}', 'wb') as out:
                 pdf_writer.write(out)
             print(f'[+] Created: {output_filename}', file=TextWrapper(text))
         else:
@@ -112,7 +119,7 @@ def pdf_select_page(path, page, app, text):
         mb.showerror("INFO", "File not found!")
 
 
-def merge_pdf(path1, path2, text):
+def merge_pdf(path1, path2, text, save_path):
     """Join two PDF files together"""
     if Path(path1).is_file() and (Path(path1).suffix == ".pdf" or Path(path1).suffix == ".PDF"):
         if Path(path2).is_file() and (Path(path2).suffix == ".pdf" or Path(path2).suffix == ".PDF"):
@@ -121,7 +128,9 @@ def merge_pdf(path1, path2, text):
             merge_file = PdfFileMerger()
             merge_file.append(path1, 'rb')
             merge_file.append(path2, 'rb')
-            merge_file.write(f"{file_name}_merged.pdf")
+            output_filename = f"{file_name}_merged.pdf"
+            with open(f'{save_path}/{output_filename}', 'wb') as out:
+                merge_file.write(out)
 
             print(f'[+] Created: {file_name}_merged.pdf', file=TextWrapper(text))
         else:
@@ -130,7 +139,7 @@ def merge_pdf(path1, path2, text):
         mb.showerror("INFO", "First file not found!")
 
 
-def pdf_delete_page(path, page_del, app, text):
+def pdf_delete_page(path, page_del, app, text, save_path):
     """Delete page from PDF file"""
     app.destroy()
     file_name = os.path.splitext(os.path.basename(path))[0]
@@ -145,7 +154,7 @@ def pdf_delete_page(path, page_del, app, text):
                     pdf_writer.addPage(pdf.getPage(page))
 
             output_filename = f'{file_name}_del_page_{page_del}.pdf'
-            with open(output_filename, 'wb') as out:
+            with open(f'{save_path}/{output_filename}', 'wb') as out:
                 pdf_writer.write(out)
             print(f'[+] Created: {output_filename}', file=TextWrapper(text))
         else:
@@ -154,19 +163,21 @@ def pdf_delete_page(path, page_del, app, text):
         mb.showerror("INFO", "File not found!")
 
 
-def pdf_to_docx(path, text):
+def pdf_to_docx(path, text, save_path):
     """Convert PDF file to DOCX file"""
     if Path(path).is_file() and (Path(path).suffix == ".pdf" or Path(path).suffix == ".PDF"):
         docx_file = os.path.splitext(os.path.basename(path))[0] + '.docx'
         cv = Converter(path)
-        cv.convert(docx_file, start=0, end=None)
+        print(save_path)
+        with open(f'{save_path}/{docx_file}', 'wb') as out:
+            cv.convert(docx_file, start=0, end=None, path=save_path)
         cv.close()
         print(f"[+] Created: {docx_file}", file=TextWrapper(text))
     else:
         mb.showerror("INFO", "File not found!")
 
 
-def encrypt_pdf(path, pas, app, text):
+def encrypt_pdf(path, pas, app, text, save_path):
     """Set password for PDF file"""
     app.destroy()
     file_writer = PdfWriter()
@@ -179,7 +190,7 @@ def encrypt_pdf(path, pas, app, text):
             file_writer.addPage(file_reader.getPage(page))
 
         file_writer.encrypt(str(pas))
-        with open(result_file_name, 'wb') as file:
+        with open(f'{save_path}/{result_file_name}', 'wb') as file:
             file_writer.write(file)
 
         print(f"[+] Created: {result_file_name}", file=TextWrapper(text))
@@ -187,7 +198,7 @@ def encrypt_pdf(path, pas, app, text):
         mb.showerror("INFO", "File not found!")
 
 
-def decrypt_pdf(path, pas, app, text):
+def decrypt_pdf(path, pas, app, text, save_path):
     """Remove password from PDF file"""
     app.destroy()
     file_writer = PdfWriter()
@@ -202,7 +213,7 @@ def decrypt_pdf(path, pas, app, text):
         for page in range(file_reader.numPages):
             file_writer.addPage(file_reader.getPage(page))
 
-        with open(result_file_name, 'wb') as file:
+        with open(f'{save_path}/{result_file_name}', 'wb') as file:
             file_writer.write(file)
 
         print(f"[+] Created: {result_file_name}", file=TextWrapper(text))
