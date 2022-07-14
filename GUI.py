effe375f199995d68c2c91bfa44af0b8fb7da6e4
luckoff <neluckoff@gmail.com>
2022-07-14 16:37:52 +0300

    Improved program visual

--- GUI.py
+++ GUI.py
@@ -45,47 +45,48 @@ class GUI:
         var3 = StringVar(self.app)
 
         # Settings 1st page
+        Label(tab1, text="PDF Converter", font=('bold', 14)).place(x=20, y=5)
         tkvar = StringVar(tab1)
         tkvar.set(option_list[0])
         var = StringVar(tab1)
 
-        w = Label(tab1, text="File Path:", font=('bold', 10), pady=20, padx=20)
-        w.grid(row=0, column=0)
-        e = Entry(tab1, textvariable=var)
-        e.grid(row=0, column=1, columnspan=2)
-        b = Button(tab1, text="Browse", command=lambda: var.set(fd.askopenfilename()))
-        b.grid(row=0, column=3, columnspan=2)
+        Label(tab1, text="File Path", font=('bold', 10)).place(x=20, y=40)
+        Entry(tab1, textvariable=var).place(y=40, x=100)
+        Button(tab1, text="Browse", command=lambda: var.set(fd.askopenfilename())).place(y=36, x=220)
 
-        OptionMenu(tab1, tkvar, *option_list).place(x=20, y=95)
+        OptionMenu(tab1, tkvar, *option_list).place(x=40, y=115)
 
         submit_button = Button(tab1, text='Get Started', command=lambda: choose_funct(tkvar.get(), var.get(),
-                                                                                 text, self.directory_save))
-        submit_button.place(x=235, y=98)
+                                                                                      text, self.directory_save))
+        submit_button.place(x=200, y=158)
 
-        Label(tab1, text="Save folder ", font=('bold', 10)).place(x=20, y=55)
-        Entry(tab1, textvariable=var3).place(x=100, y=55)
-        Button(tab1, text="Browse", command=lambda: self.choose_directory(var3)).place(x=225, y=51)
+        Label(tab1, text="Save folder ", font=('bold', 10)).place(x=20, y=75)
+        Entry(tab1, textvariable=var3).place(x=100, y=75)
+        Button(tab1, text="Browse", command=lambda: self.choose_directory(var3)).place(x=220, y=71)
 
         # Setting 2nd page
+        Label(tab2, text="Image Converter", font=('bold', 14)).place(x=20, y=5)
         var2 = StringVar(tab2)
 
         button = Button(tab2, text="Choose Images", command=lambda: self.get_images())
-        button.place(x=20, y=60)
+        button.place(x=20, y=120)
 
-        button2 = Button(tab2, text="Start Converting", command=lambda: image_in_pdf(self.files, var2.get(), text))
-        button2.place(x=180, y=60)
+        button2 = Button(tab2, text="Start Converting", command=lambda: image_in_pdf(self.files, var2.get(),
+                                                                                     text, var3.get()))
+        button2.place(x=180, y=120)
 
-        Label(tab2, text="Enter Author Name: ", pady=20, padx=20, font=('bold', 10)).grid(row=0, column=0)
-        Entry(tab2, textvariable=var2).grid(row=0, column=1)
-        Label(tab2, text="* Just select all images", font=("default", 8)).place(x=20, y=85)
+        Label(tab2, text="Enter Author Name: ", font=('bold', 10)).place(x=20, y=40)
+        Entry(tab2, textvariable=var2).place(y=40, x=150)
+        Label(tab2, text="* Just select all images", font=("default", 8)).place(x=20, y=145)
 
-        Label(tab2, text="Save folder ").place(x=20, y=120)
-        Entry(tab2, textvariable=var3).place(x=90, y=120)
-        Button(tab2, text="Browse", command=lambda: self.choose_directory(var3)).place(x=214, y=116)
+        Label(tab2, text="Save folder ", font=('bold', 10)).place(x=20, y=80)
+        Entry(tab2, textvariable=var3).place(x=100, y=80)
+        Button(tab2, text="Browse", command=lambda: self.choose_directory(var3)).place(x=224, y=76)
 
         # All App
         text = Text(self.app)
         text.place(x=310, y=30, height=310, width=420)
+        Label(self.app, text="https://github.com/neluckoff/pdf_converter").place(x=5, y=320)
 
         tab_control.pack(expand=1, fill='both')
 
