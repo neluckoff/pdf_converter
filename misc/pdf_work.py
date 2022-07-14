21017098c87550c17fc8e99b244112880bb25149
luckoff <neluckoff@gmail.com>
2022-07-14 16:36:55 +0300

    Added encryption check

--- misc/pdf_work.py
+++ misc/pdf_work.py
@@ -24,11 +24,11 @@ def choose_funct(choosing, path, text, save_path):
             y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
             new_app.wm_geometry("+%d+%d" % (x, y))
             var1 = StringVar(new_app)
-            entry = Entry(new_app, textvariable=var1)
-            entry.grid(row=0, column=1)
-            Button(new_app, text='Delete', command=lambda: pdf_delete_page(path, int(entry.get()),
-                                                                           new_app, text, save_path))\
-                .grid(row=0, column=2)
+            Label(new_app, text="Enter page to delete", font=('bold', 10)).place(x=10, y=5)
+            Entry(new_app, textvariable=var1).place(x=12, y=35)
+            Button(new_app, text='Delete',
+                   command=lambda: pdf_delete_page(path, int(var1.get()),
+                                                   new_app, text, save_path)).place(y=31, x=120)
             new_app.mainloop()
         elif choosing == "Select one page from PDF":
             new_app = Tk()
@@ -38,11 +38,11 @@ def choose_funct(choosing, path, text, save_path):
             y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
             new_app.wm_geometry("+%d+%d" % (x, y))
             var1 = StringVar(new_app)
-            entry = Entry(new_app, textvariable=var1)
-            entry.grid(row=0, column=1)
-            Button(new_app, text='Select', command=lambda: pdf_select_page(path, int(entry.get()), new_app,
-                                                                           text, save_path))\
-                .grid(row=0, column=2)
+            Label(new_app, text="Page to save as a separate PDF", font=('bold', 10)).place(x=10, y=5)
+            Entry(new_app, textvariable=var1).place(x=12, y=35)
+            Button(new_app, text='Select',
+                   command=lambda: pdf_select_page(path, int(var1.get()),
+                                                   new_app, text, save_path)).place(y=31, x=120)
             new_app.mainloop()
         elif choosing == "Merge two PDF files":
             new_file = fd.askopenfilename()
@@ -55,11 +55,11 @@ def choose_funct(choosing, path, text, save_path):
             y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
             new_app.wm_geometry("+%d+%d" % (x, y))
             var1 = StringVar(new_app)
-            entry = Entry(new_app, textvariable=var1)
-            entry.grid(row=0, column=1)
-            Button(new_app, text='Encrypt', command=lambda: encrypt_pdf(path, entry.get(),
-                                                                        new_app, text, save_path)) \
-                .grid(row=0, column=2)
+            Label(new_app, text="Enter password to encrypt PDF file", font=('bold', 10)).place(x=10, y=5)
+            Entry(new_app, textvariable=var1).place(x=12, y=35)
+            Button(new_app, text='Encrypt',
+                   command=lambda: encrypt_pdf(path, var1.get(),
+                                               new_app, text, save_path)).place(y=31, x=120)
             new_app.mainloop()
         elif choosing == "Decrypt PDF file":
             new_app = Tk()
@@ -69,11 +69,11 @@ def choose_funct(choosing, path, text, save_path):
             y = (new_app.winfo_screenheight() - new_app.winfo_reqheight()) / 2
             new_app.wm_geometry("+%d+%d" % (x, y))
             var1 = StringVar(new_app)
-            entry = Entry(new_app, textvariable=var1)
-            entry.grid(row=0, column=1)
-            Button(new_app, text='Decrypt', command=lambda: decrypt_pdf(path, entry.get(),
-                                                                        new_app, text, save_path)) \
-                .grid(row=0, column=2)
+            Label(new_app, text="Enter password to decrypt PDF file", font=('bold', 10)).place(x=10, y=5)
+            Entry(new_app, textvariable=var1).place(x=12, y=35)
+            Button(new_app, text='Decrypt',
+                   command=lambda: decrypt_pdf(path, var1.get(),
+                                               new_app, text, save_path)).place(y=31, x=120)
             new_app.mainloop()
     else:
         mb.showerror("ERROR", "Specify the path to save the file!")
@@ -83,18 +83,21 @@ def pdf_info(path, text):
     """Find out all the information about the PDF file"""
     if Path(path).is_file() and (Path(path).suffix == ".pdf" or Path(path).suffix == ".PDF"):
         file_name = os.path.splitext(os.path.basename(path))[0]
-        with open(path, 'rb') as file:
-            pdf = PdfReader(file)
-            pages = pdf.getNumPages()
-            pdf_i = pdf.getDocumentInfo()
+        try:
+            with open(path, 'rb') as file:
+                pdf = PdfReader(file)
+                pages = pdf.getNumPages()
+                pdf_i = pdf.getDocumentInfo()
 
-        print(f'File name: {file_name}', file=TextWrapper(text))
-        print(f'Author: {pdf_i.author}', file=TextWrapper(text))
-        print(f'Creator: {pdf_i.creator}', file=TextWrapper(text))
-        print(f'Title: {pdf_i.title}', file=TextWrapper(text))
-        print(f'Pages: {pages}', file=TextWrapper(text))
+            print(f'File name: {file_name}', file=TextWrapper(text))
+            print(f'Author: {pdf_i.author}', file=TextWrapper(text))
+            print(f'Creator: {pdf_i.creator}', file=TextWrapper(text))
+            print(f'Title: {pdf_i.title}', file=TextWrapper(text))
+            print(f'Pages: {pages}', file=TextWrapper(text))
+        except Exception as ex:
+            mb.showerror("ERROR", f'{ex}')
     else:
-        mb.showerror("INFO", "File not found!")
+        mb.showinfo("INFO", "File not found!")
 
 
 def pdf_select_page(path, page, app, text, save_path):
@@ -102,19 +105,22 @@ def pdf_select_page(path, page, app, text, save_path):
     app.destroy()
     file_name = os.path.splitext(os.path.basename(path))[0]
     if Path(path).is_file() and (Path(path).suffix == ".pdf" or Path(path).suffix == ".PDF"):
-        pdf = PdfReader(path)
-        pages = pdf.getNumPages()
+        try:
+            pdf = PdfReader(path)
+            pages = pdf.getNumPages()
 
-        if 0 < page < pages:
-            pdf_writer = PdfWriter()
-            pdf_writer.addPage(pdf.getPage(page-1))
+            if 0 < page < pages:
+                pdf_writer = PdfWriter()
+                pdf_writer.addPage(pdf.getPage(page - 1))
 
-            output_filename = f'{file_name}_page_{page}.pdf'
-            with open(f'{save_path}/{output_filename}', 'wb') as out:
-                pdf_writer.write(out)
-            print(f'[+] Created: {output_filename}', file=TextWrapper(text))
-        else:
-            print(f"[INFO] There are only {pages-1} pages in the file", file=TextWrapper(text))
+                output_filename = f'{file_name}_page_{page}.pdf'
+                with open(f'{save_path}/{output_filename}', 'wb') as out:
+                    pdf_writer.write(out)
+                print(f'[+] Created: {output_filename}', file=TextWrapper(text))
+            else:
+                mb.showinfo('INFO', f"[INFO] There are only {pages - 1} pages in the file")
+        except Exception as ex:
+            mb.showerror("ERROR", f'{ex}')
     else:
         mb.showerror("INFO", "File not found!")
 
@@ -123,16 +129,19 @@ def merge_pdf(path1, path2, text, save_path):
     """Join two PDF files together"""
     if Path(path1).is_file() and (Path(path1).suffix == ".pdf" or Path(path1).suffix == ".PDF"):
         if Path(path2).is_file() and (Path(path2).suffix == ".pdf" or Path(path2).suffix == ".PDF"):
-            file_name = os.path.splitext(os.path.basename(path1))[0]
+            try:
+                file_name = os.path.splitext(os.path.basename(path1))[0]
 
-            merge_file = PdfFileMerger()
-            merge_file.append(path1, 'rb')
-            merge_file.append(path2, 'rb')
-            output_filename = f"{file_name}_merged.pdf"
-            with open(f'{save_path}/{output_filename}', 'wb') as out:
-                merge_file.write(out)
+                merge_file = PdfFileMerger()
+                merge_file.append(path1, 'rb')
+                merge_file.append(path2, 'rb')
+                output_filename = f"{file_name}_merged.pdf"
+                with open(f'{save_path}/{output_filename}', 'wb') as out:
+                    merge_file.write(out)
 
-            print(f'[+] Created: {file_name}_merged.pdf', file=TextWrapper(text))
+                print(f'[+] Created: {file_name}_merged.pdf', file=TextWrapper(text))
+            except Exception as ex:
+                mb.showerror("ERROR", f'{ex}')
         else:
             mb.showerror("INFO", "Second file not found!")
     else:
@@ -144,21 +153,24 @@ def pdf_delete_page(path, page_del, app, text, save_path):
     app.destroy()
     file_name = os.path.splitext(os.path.basename(path))[0]
     if Path(path).is_file() and (Path(path).suffix == ".pdf" or Path(path).suffix == ".PDF"):
-        pdf = PdfReader(path)
-        pages = pdf.getNumPages()
-        pdf_writer = PdfWriter()
+        try:
+            pdf = PdfReader(path)
+            pages = pdf.getNumPages()
+            pdf_writer = PdfWriter()
 
-        if 0 < page_del < pages:
-            for page in range(pdf.getNumPages()):
-                if page != page_del-1:
-                    pdf_writer.addPage(pdf.getPage(page))
+            if 0 < page_del < pages:
+                for page in range(pdf.getNumPages()):
+                    if page != page_del - 1:
+                        pdf_writer.addPage(pdf.getPage(page))
 
-            output_filename = f'{file_name}_del_page_{page_del}.pdf'
-            with open(f'{save_path}/{output_filename}', 'wb') as out:
-                pdf_writer.write(out)
-            print(f'[+] Created: {output_filename}', file=TextWrapper(text))
-        else:
-            print(f"[INFO] There are only {pages-1} pages in the file", file=TextWrapper(text))
+                output_filename = f'{file_name}_del_page_{page_del}.pdf'
+                with open(f'{save_path}/{output_filename}', 'wb') as out:
+                    pdf_writer.write(out)
+                print(f'[+] Created: {output_filename}', file=TextWrapper(text))
+            else:
+                print(f"[INFO] There are only {pages - 1} pages in the file", file=TextWrapper(text))
+        except Exception as ex:
+            mb.showerror("ERROR", f'{ex}')
     else:
         mb.showerror("INFO", "File not found!")
 
@@ -166,13 +178,14 @@ def pdf_delete_page(path, page_del, app, text, save_path):
 def pdf_to_docx(path, text, save_path):
     """Convert PDF file to DOCX file"""
     if Path(path).is_file() and (Path(path).suffix == ".pdf" or Path(path).suffix == ".PDF"):
-        docx_file = os.path.splitext(os.path.basename(path))[0] + '.docx'
-        cv = Converter(path)
-        print(save_path)
-        with open(f'{save_path}/{docx_file}', 'wb') as out:
-            cv.convert(docx_file, start=0, end=None, path=save_path)
-        cv.close()
-        print(f"[+] Created: {docx_file}", file=TextWrapper(text))
+        try:
+            docx_file = os.path.splitext(os.path.basename(path))[0] + '.docx'
+            cv = Converter(path)
+            cv.convert(f'{save_path}/{docx_file}', start=0, end=None)
+            cv.close()
+            print(f"[+] Created: {docx_file}", file=TextWrapper(text))
+        except Exception as ex:
+            mb.showerror("ERROR", f'File has not been decrypted')
     else:
         mb.showerror("INFO", "File not found!")
 
@@ -208,14 +221,17 @@ def decrypt_pdf(path, pas, app, text, save_path):
     if Path(path).is_file() and (Path(path).suffix == ".pdf" or Path(path).suffix == ".PDF"):
         file_reader = PdfReader(path)
         if file_reader.is_encrypted:
-            file_reader.decrypt(str(pas))
+            try:
+                file_reader.decrypt(str(pas))
 
-        for page in range(file_reader.numPages):
-            file_writer.addPage(file_reader.getPage(page))
+                for page in range(file_reader.numPages):
+                    file_writer.addPage(file_reader.getPage(page))
 
-        with open(f'{save_path}/{result_file_name}', 'wb') as file:
-            file_writer.write(file)
+                with open(f'{save_path}/{result_file_name}', 'wb') as file:
+                    file_writer.write(file)
 
-        print(f"[+] Created: {result_file_name}", file=TextWrapper(text))
+                print(f"[+] Created: {result_file_name}", file=TextWrapper(text))
+            except Exception as ex:
+                mb.showerror("ERROR", f"{ex}\nCheck your password and try again")
     else:
         mb.showerror("INFO", "File not found!")
