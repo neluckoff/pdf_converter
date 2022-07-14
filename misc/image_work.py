9d3ec2fa182c8aa9b4bc929db73d447cad129578
luckoff <neluckoff@gmail.com>
2022-07-14 16:37:26 +0300

    Added save to selected folder

--- misc/image_work.py
+++ misc/image_work.py
@@ -4,20 +4,23 @@ from tkinter import messagebox as mb
 from misc.text_wrapper import TextWrapper
 
 
-def image_in_pdf(image_list, author, text):
+def image_in_pdf(image_list, author, text, save_path):
     """Convert all images in pdf file"""
     if image_list is not None:
-        image_arr = []
-        for path in image_list:
-            if Path(path).suffix == ".png" or Path(path).suffix == ".jpg" or Path(path).suffix == ".jpeg":
-                im1 = (Image.open(path)).convert('RGB')
-                image_arr.append(im1)
-            else:
-                mb.showwarning(f"WARNING!", f"The file is not a photo!\n\n{path}")
+        if save_path is not None:
+            image_arr = []
+            for path in image_list:
+                if Path(path).suffix == ".png" or Path(path).suffix == ".jpg" or Path(path).suffix == ".jpeg":
+                    im1 = (Image.open(path)).convert('RGB')
+                    image_arr.append(im1)
+                else:
+                    mb.showwarning(f"WARNING!", f"The file is not a photo!\n\n{path}")
 
-        image_arr[0].save('Images.pdf', save_all=True,
-                          append_images=image_arr[1:], creator="github.com/neluckoff/pdf_converter",
-                          title="Images in PDF file", author=str(author))
-        print('[+] Created: images.pdf', file=TextWrapper(text))
+            image_arr[0].save(f'{save_path}/Images.pdf', save_all=True,
+                              append_images=image_arr[1:], creator="github.com/neluckoff/pdf_converter",
+                              title="Images in PDF file", author=str(author))
+            print('[+] Created: Images.pdf', file=TextWrapper(text))
+        else:
+            mb.showerror("ERROR", "Specify the path to save the file!")
     else:
         mb.showwarning(f"WARNING!", f"You have not selected any images")
